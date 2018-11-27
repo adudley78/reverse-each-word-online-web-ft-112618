cc3aa4140ceb588e687cbfe4e04fce023b157ca8
Adam Dudley <dudleyadam@gmail.com>
2018-11-27 19:38:07 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,4 @@
 def reverse_each_word(sentence1, sentence2)
-  sentence = [ ] #creates an empty array to receive the sentence
-  words.each do
-  end
+  sentence1.reverse! = some_sentence
+    end
 end
