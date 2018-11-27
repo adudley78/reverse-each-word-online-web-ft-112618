028cd70c86d13e3b147be6d02b0ad0a227d7cedf
Adam Dudley <dudleyadam@gmail.com>
2018-11-27 20:55:31 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,6 +8,7 @@ def reverse_each_word(sentences)
 end
 
 def reverse_each_word(sentences)
+  my strings = [ ]
   split_sentences = sentences.split(" ")
   split_sentences.collect do |sentence|
     my_strings << sentence.reverse
