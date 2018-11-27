bcd89db39e9ceaf1951b585fc730cfb93cfe3382
Adam Dudley <dudleyadam@gmail.com>
2018-11-27 20:28:00 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentences)
   my_strings = [ ]
   split_sentences = sentences.split
-  sentences.each do |sentences|
+  split_sentences.each do |sentences|
   my_strings = sentences.reverse!
   end
   my_strings
